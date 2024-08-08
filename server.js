07a77d312b4794ea36821bba112d43c2883f6dbf
babujal <dercamar1@gmail.com>
2024-08-07 20:41:00 -0400

    Done

--- server.js
+++ server.js
@@ -2,62 +2,71 @@ const express = require('express');
 const app = express();
 
 const RESTAURANT = {
-    name: 'The Green Byte Bistro',
-    isOpen: true,
-    address: '742 Evergreen Rd, Mapleview, OS 45502',
-    phone: '555-321-9876',
-    menu: [
-      { 
-        id: 1,
-        name: 'Quantum Quinoa Mushroom Burger',
-        price: 13.00,
-        rating: 4,
-        category: 'mains',
-        details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
-      },
-      { 
-        id: 2,
-        name: 'Binary Berry Cheesecake',
-        price: 10.11,
-        rating: 3,
-        category: 'desserts',
-        details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
-      },
-      { 
-        id: 3,
-        name: 'Recursive Rigatoni',
-        price: 17.00,
-        rating: 5,
-        category: 'mains',
-        details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
-      },
-      { 
-        id: 4,
-        name: 'Pumpkin Pi Squared',
-        price: 3.14,
-        rating: 5,
-        category: 'desserts',
-        details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
-      },
-      { 
-        id: 5,
-        name: 'Fibonacci String Bean Fries',
-        price: 11.23,
-        rating: 5,
-        category: 'sides',
-        details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
-      }
-    ]
-  };
-  const menu = RESTAURANT.menu
-  
+  name: 'The Green Byte Bistro',
+  isOpen: true,
+  address: '742 Evergreen Rd, Mapleview, OS 45502',
+  phone: '555-321-9876',
+  menu: [
+    {
+      id: 1,
+      name: 'Quantum Quinoa Mushroom Burger',
+      price: 13.00,
+      rating: 4,
+      category: 'mains',
+      details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+    },
+    {
+      id: 2,
+      name: 'Binary Berry Cheesecake',
+      price: 10.11,
+      rating: 3,
+      category: 'desserts',
+      details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+    },
+    {
+      id: 3,
+      name: 'Recursive Rigatoni',
+      price: 17.00,
+      rating: 5,
+      category: 'mains',
+      details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+    },
+    {
+      id: 4,
+      name: 'Pumpkin Pi Squared',
+      price: 3.14,
+      rating: 5,
+      category: 'desserts',
+      details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+    },
+    {
+      id: 5,
+      name: 'Fibonacci String Bean Fries',
+      price: 11.23,
+      rating: 5,
+      category: 'sides',
+      details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
+    }
+  ]
+};
+const menu = RESTAURANT.menu
+
 
 app.get('/', (req, res) => {
-  res.render('home.ejs', {RESTAURANT});
+  res.render('home.ejs', { RESTAURANT });
 });
 
 app.get('/menu', (req, res) => {
-    res.render('menu.ejs', {menu});
-  });
+  const allMenu = RESTAURANT.menu
+  res.render('menu.ejs', { allMenu });
+});
+
+app.get('/menu/:category', (req, res) => {
+  const category = req.params.category
+  const capitalizedCategory = category[0].toUpperCase() + category.slice(1)
+  const menus = RESTAURANT.menu
+  const categoryMenu = menus.filter((el) => el.category === category)
+  res.render('category.ejs', { categoryMenu, capitalizedCategory, category });
+});
 
 app.listen(3000);
